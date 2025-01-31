1e2c1980bdd7044807303d11b4901756dd74b6cc
Rruubeenn23 <160345061+Rruubeenn23@users.noreply.github.com>
2025-01-31 11:32:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session as flask_session
 from database import session, Usuario
 import logging
+import os
 logging.basicConfig(level=logging.DEBUG)
 from database import session, Usuario, Personaje
 
@@ -166,5 +167,6 @@ def characters():
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Railway asigna dinámicamente el puerto
+    app.run(host="0.0.0.0", port=port, debug=True)
